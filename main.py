c1cb8ee40173762dc384c0e70b26168b1ca7a41e
shiva ganji <135611043+ShivaGanji88@users.noreply.github.com>
2023-09-09 17:35:46 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+travel_log = [
+{
+  "country": "France",
+  "visits": 12,
+  "cities": ["Paris", "Lille", "Dijon"]
+},
+{
+  "country": "Germany",
+  "visits": 5,
+  "cities": ["Berlin", "Hamburg", "Stuttgart"]
+},
+]
+#🚨 Do NOT change the code above
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+def add_new_country(country_visited, times_visited, cities_visited):
+    new_dictionery = {}
+    new_dictionery["country"] = country_visited
+    new_dictionery["visites"] = times_visited
+    new_dictionery["cities"] = cities_visited
+    travel_log.append(new_dictionery)
+
+
+
+#🚨 Do not change the code below
+add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log)
